edb787df58b0a36e92a176d2e7fa3cca52cdd3d7
Brock <grinstba@rose-hulman.edu>
2019-11-07 15:20:32 -0800

    Added FFTDataTest

--- test/FFTdataTest.py
+++ test/FFTdataTest.py
@@ -1,24 +1,6 @@
-"""
-    Notebook for streaming data from a microphone in realtime
-
-    audio is captured using pyaudio
-    then converted from binary data to ints using struct
-    then displayed using matplotlib
-
-    scipy.fftpack computes the FFT
-
-    if you don't have pyaudio, then run
-
-    >>> pip install pyaudio
-
-    note: with 2048 samples per chunk, I'm getting 20FPS
-    when also running the spectrum, its about 15FPS
-"""
 import matplotlib.pyplot as plt
 import numpy as np
 import pyaudio
-# from pyqtgraph.Qt import QtGui, QtCore
-# import pyqtgraph as pg
 import struct
 from scipy.fftpack import fft
 import sys
@@ -34,6 +16,7 @@ class AudioStream(object):
         self.CHANNELS = 1
         self.RATE = 44100
         self.pause = False
+        self.numBands = 32
 
         # stream object
         self.p = pyaudio.PyAudio()
@@ -45,18 +28,43 @@ class AudioStream(object):
             output=True,
             frames_per_buffer=self.CHUNK,
         )
-        self.fft()
+        self.startSpectrum()
 
-    def fft(self):
+    def createBands(self, data):
+        bandData = [0] * self.numBands
+        bandSize = int(self.CHUNK / self.numBands)
+        for k in range(self.numBands):
+            sum = 0
+            for i in range(bandSize):
+                sum += data[k*bandSize + i]
+
+            bandData[k] = sum / bandSize
+
+        return bandData
+            
+    def startSpectrum(self):
+
+        x = np.linspace(0,self.numBands - 1,self.numBands)
+        y = [0] * self.numBands
+        plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        line1, = ax.plot(x, y, 'b-')
+
         while True:
             data = self.stream.read(self.CHUNK, False)
             data_int = struct.unpack(str(2 * self.CHUNK) + 'B', data)
             data_np = np.array(data_int, dtype='b')[::2] + 128
 
-            yf = fft(data_int, 64)
-            yff = np.abs(yf[0:self.CHUNK]) / (128 * self.CHUNK)
-            print(yff)
+            yf = fft(data_int)
+            yfData = np.abs(yf[0:self.CHUNK]) / (128 * self.CHUNK)
 
+            bandData = self.createBands(yfData)
+            print(len(bandData))
+            line1.set_ydata(bandData)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+            
 
 if __name__ == '__main__':
     AudioStream()
